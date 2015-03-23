72867ffd3370b69ab85a1c87bf25f7faa0cdcb6e
unknown <filinale@KIEDWM417488.INTRANET.BARCAPINT.com>
2015-03-23 14:09:48 +0200

    Reply

--- practice/IDisposableImplementation/IDisposableImplementation/MemoryStreamLogger.cs
+++ practice/IDisposableImplementation/IDisposableImplementation/MemoryStreamLogger.cs
@@ -7,8 +7,8 @@ namespace Epam.NetMentoring
 	{
 		private readonly FileStream _memoryStream;
 		private readonly StreamWriter _streamWriter;
-	    private const string LogFile = "log.txt";
-	    private bool _disposed;
+		private const string LogFile = "log.txt";
+		private bool _disposed;
 
 		public MemoryStreamLogger()
 		{
@@ -25,53 +25,48 @@ namespace Epam.NetMentoring
 			_streamWriter.Write(message);
 		}
 
+		
+		
+		//protected virtual void Dispose(bool disposing)
+		//{
+		//    if (!_disposed)
+		//    {
+		//        //IT: do not change anything, just anwer the question - why the code you writen inside block if (disposing) ...
+		//        //IT: what will happen if we remove if and run code in any case?
+				
+				
+		//    //F: There are 2 scenarios:
+		//// If disposing equals true, the method has been called directly or indirectly by a user's code. 
+		////Managed and unmanaged resources can be disposed. 
+		//// If disposing equals false, the method has been called from inside the finalizer and we should not reference other objects. Only unmanaged resources must be disposed. 
+				
+		//        if (disposing)
+		//        {
+		//            if (_streamWriter != null)
+		//                _streamWriter.Dispose();
+						
+		//            if (_memoryStream != null)
+		//                _memoryStream.Dispose();
+		//        }
+				
+		//         _disposed = true;
+		//    }
+		//}
+
 		//BK: If you don't have unmanaged resources in your class and you're almost 100% sure they won't be incorporated soon, try to avoid (bool disposing) There is no much sense in using that, while we don't have unmanaged resources
 		//F: 
-		//protected virtual void Dispose(bool disposing)
-	//        {
-	//            if (!_disposed)
-	//            {
-				
-	//                    if (_streamWriter != null)
-	//                        _streamWriter.Dispose();
-						
-	//                    if (_memoryStream != null)
-	//                        _memoryStream.Dispose();
-
-	//                 _disposed = true;
-	//            }
-	//        }
-		
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!_disposed)
-			{
-				//IT: do not change anything, just anwer the question - why the code you writen inside block if (disposing) ...
-				//IT: what will happen if we remove if and run code in any case?
-				
-				
-			//F: There are 2 scenarios:
-		// If disposing equals true, the method has been called directly or indirectly by a user's code. 
-		//Managed and unmanaged resources can be disposed. 
-		// If disposing equals false, the method has been called from inside the finalizer and we should not reference other objects. Only unmanaged resources must be disposed. 
-				
-				if (disposing)
-				{
-					if (_streamWriter != null)
-						_streamWriter.Dispose();
-						
-					if (_memoryStream != null)
-						_memoryStream.Dispose();
-				}
-				
-				 _disposed = true;
-			}
-		}
-
 		public void Dispose()
 		{
-			Dispose(true);
-		GC.SuppressFinalize(this); 
+			if (_disposed) return;
+
+			if (_streamWriter != null)
+				_streamWriter.Dispose();
+
+			if (_memoryStream != null)
+				_memoryStream.Dispose();
+
+			_disposed = true;
+			GC.SuppressFinalize(this);
 		}
 	}
 }
--- practice/IDisposableImplementation/IDisposableImplementation/Program.cs
+++ practice/IDisposableImplementation/IDisposableImplementation/Program.cs
@@ -3,28 +3,32 @@
 namespace Epam.NetMentoring
 {
     //BK: not optimal Disposal of resources here. Please have a look closer
-    //F: I updated the code to use only one instance of MemoryStreamLogger and disposed it after iteration completed
+    //F: In this case I don't see much sense in WriteLog method at all. I would did the following: 
     internal class Program
     {
-        private static readonly MemoryStreamLogger Logger = new MemoryStreamLogger();
         private static void Main(string[] args)
         {
-     
-            for(var i = 0; i < 10000; i++)
-                WriteLog("Interation number #" + i);
-            Logger.Dispose();
+
+            using (MemoryStreamLogger logger = new MemoryStreamLogger())
+                for(var i = 0; i < 10000; i++)
+                   logger.Log("Interation number #" + i);
+
+            
+            //WriteLog("Interation number #" + i);
+            //Logger.Dispose();
+
             Console.WriteLine("Finished");
             Console.ReadKey();
             
         }
-        private static void WriteLog(string str)
-        {
-            //using (var logger = new MemoryStreamLogger())
-            //{
-            //    logger.Log(str);
-            //}
-            Logger.Log(str);
-        }
+        //private static void WriteLog(string str)
+        //{
+        //    using (var logger = new MemoryStreamLogger())
+        //    {
+        //        logger.Log(str);
+        //    }
+            
+        //}
 
 
     }
