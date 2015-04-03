9bac4aa35af17644a86706bc791890e29a9c68c0
unknown <filinale@KIEDWM417488.INTRANET.BARCAPINT.com>
2015-04-03 16:14:54 +0300

    Task #3

---
+++ practice/IDisposable/Self-Recovering2/Self-Recovering/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Self_Recovering
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please make your choice:\n");
+                Console.WriteLine("\tPress 1 to create self recovering object");
+                Console.WriteLine("\tPress 2 to Exit app\n");
+                
+                int caseSwitch;
+                Int32.TryParse(Console.ReadLine(), out caseSwitch);
+
+                switch (caseSwitch)
+                {
+                    case 1:
+                        var c = new SelfRecoveringObj();
+                        Console.WriteLine("Object was created. Press Enter");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ practice/IDisposable/Self-Recovering2/Self-Recovering/SelfRecoveringObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Self_Recovering
+{
+    public class SelfRecoveringObj
+    {
+        private readonly List<SelfRecoveringObj> _pool;
+
+        public SelfRecoveringObj()
+        {
+            RegenerationCount = 0;
+            _pool = new List<SelfRecoveringObj>();
+
+        }
+
+        public int RegenerationCount { get; private set; }
+
+        ~SelfRecoveringObj()
+        {
+            RegenerationCount++;
+            _pool.Add(this);
+            GC.ReRegisterForFinalize(this);
+            Console.WriteLine("Object {0} restored {1} times", GetHashCode(), RegenerationCount);
+        }
+
+        
+    }
+}
