7793e04c411222b5fa61b6d0c9b0b2aee18c2aaa
fm107 <fm107@ya.ru>
2015-04-20 16:18:51 +0300

    Reply

--- practice/Data Structure/Hash Table/Hash Table/Program.cs
+++ practice/Data Structure/Hash Table/Hash Table/Program.cs
@@ -6,7 +6,7 @@ namespace Epam.NetMentoring.DataStructures.HashTable
     {
         static void Main(string[] args)
         {
-            HashTable ht = new HashTable(10);
+            HashTable ht = new HashTable(10); //HashTable with fixed size
             ht.Add("1","fdgdgfdg");
             ht.Add("2","yuu");
             ht.Add("3", "sfr");
@@ -17,7 +17,7 @@ namespace Epam.NetMentoring.DataStructures.HashTable
             Console.WriteLine(ht.Contains("4"));
             ht.TryGet("2", out test);
             Console.WriteLine(test);
-           ht["1"] = null;
+            ht["1"] = null;
            
             Console.Read();
         }
