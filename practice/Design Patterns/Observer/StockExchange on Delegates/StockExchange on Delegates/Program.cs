af94ee36dd4298e24dbebc480b4ff0ae36d03196
Exsabot <nikospamemail@gmail.com>
2015-06-23 17:42:38 +0300

    Reviewed by BK

--- practice/Design Patterns/Observer/StockExchange on Delegates/StockExchange on Delegates/Program.cs
+++ practice/Design Patterns/Observer/StockExchange on Delegates/StockExchange on Delegates/Program.cs
@@ -6,6 +6,9 @@ namespace Epam.NetMentoring.Patterns.Observer.StockExchangeOnDelegates
     {
         static void Main(string[] args)
         {
+            //BK: The main idea of writing stovk exchnage on delegates is that there is no attach/detach actions. According to a task market should have a delegate, to which all your Investors will subscribe. THe benefit of using delegates events is that subscribers shouldn't have single common interface.
+            //BK: The same story is about Events. PLease reimplement both and avoid using interfaces here for subscribers.
+
             Stock epam = new Stock("epam", 14.00, 7.00);
             StockMarketing market = new StockMarketing(epam);
             IObserver alex = new Investor("Alex", 12.00, epam);
