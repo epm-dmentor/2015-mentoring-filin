06309619e4350d7e7c90c768e80aec1121f11c34
Alexander Filin <fm1070@gmail.com>
2015-07-14 18:32:58 +0300

    MVVM and Commanding

---
+++ practice/Patterns/Command/Command/ViewModel/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Command.ViewModel
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ practice/Patterns/Command/Command/ViewModel/ProcessList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Command.ViewModel
+{
+    internal class ProcessList
+    {
+        public string RunCommand()
+        {
+            string ouput = string.Empty;
+            try
+            {
+                Process[] processes = Process.GetProcesses();
+                return
+                    processes.Aggregate(ouput,
+                        (currentProcess, nextProcess) => currentProcess + Environment.NewLine + nextProcess).Trim();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
