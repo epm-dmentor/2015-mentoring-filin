6b5300928be6dbf8a009f83b86b9ac34e03391bd
fm107 <fm107@ya.ru>
2015-07-22 00:06:18 +0300

    Template

--- practice/Design Patterns/Template Method/Template Method/Template.cs
+++ practice/Design Patterns/Template Method/Template Method/Template.cs
@@ -4,7 +4,7 @@ namespace Epam.NetMentoring.DesignPatterns.Template_Method
 {
     internal class Template
     {
-        public IEnumerable<Email> Emails;
+        public IEnumerable<Email> Emails { get; set; }
         public IEnumerable<User> Users { get; set; }
         public bool IsRegistered { get; set; }
         public string Name { get; set; }
